06b5c7917beb100464723700ee9073d27384c0f9
Jason Pescione <j@Jasons-MacBook-Air.local>
2019-06-12 07:13:22 -0400

    boilerplate

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# js-types
+
+asfasdfasdfadfs
+
+**engines: node >= 9.x**
+
+## Installation
+```
+npm i --save js-types
+```
+
+## Example
+```javascript
+const $ = require('js-types');
+
+```
+
+## API
+
+asfasdfasdfadfs
+
+```javascript
+const $ = require('js-types');
+```
+
+## License
+
+MIT
---
+++ tests/main.test.js
@@ -0,0 +1,20 @@
+const lib = require('../lib');
+const isEqual = require('js-isequal');
+
+const should = require('chai').should();
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+// 2 error cases. invalid range and missing param//
+describe('js- tests', () => {
+  beforeEach(() => {
+
+  });
+
+  describe('', () => {
+    it('', () => {
+      // expect(isEqual()).to.be.true;
+    });
+  });
+
+});
