bd0fcffb14c9cc7c8e702e49a439feae8ae1fa11
Jason Pescione <j@Jasons-MacBook-Air.local>
2019-06-16 14:17:45 -0400

    tests in progress

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,19 +1,23 @@
-const lib = require('../lib');
+const type = require('../lib');
 
 const should = require('chai').should();
 const expect = require('chai').expect;
 const assert = require('chai').assert;
 
-// 2 error cases. invalid range and missing param//
-describe('js- tests', () => {
-  beforeEach(() => {
+var func = (a, b, c, d) => {};
+var funcThrow = type.throw(func, 'string', ['number', 'boolean'], 'object, array', value => null);
+var funcError = type.error(func, 'string', ['number', 'boolean'], 'object, array', value => null);
+var funcFail  = type.fail(func, 'string', ['number', 'boolean'], 'object, array', value => null);
 
-  });
+var types = ['string', 'number', 'boolean', 'object', 'array', 'undefined', 'null', 'function', 'date', 'NaN', 'error', 'symbol'];
+var valid = ['a', 1, true, {}, [], undefined, null, () => {}, new Date(), NaN, new Error(), Symbol()];
+var invalid = [1, 'a', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
-  describe('', () => {
-    it('', () => {
-      // expect(sdf()).to.be.true;
+// test lock.
+describe('js-type tests', () => {
+  describe('.throw()', () => {
+    it ('should', () => {
+
     });
   });
-
 });
